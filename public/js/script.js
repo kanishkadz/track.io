c2c3247bb9506cc3f837bb66c69046fb6a943563
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-07 19:41:52 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -20,3 +20,8 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 const markers = {};
+
+socket.on("receive-location", (data) => {
+    const {id, latitude, longitude} = data;
+    map.setView([latitude, longitude]);
+});
