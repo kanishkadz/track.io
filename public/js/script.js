0f149c29e023ef707cd6e8ee452d2bf1cfbf2f0d
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-07 19:29:29 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -4,5 +4,8 @@ const socket = io();
 if(navigator.geolocation){
     navigator.geolocation.watchPosition((position) => {
         const { latitude, longitude } = position.coords;
-    })
+        socket.emit("send-location", {latitude, longitude});
+    }, (error) => {
+        console.error(error);
+    });
 }
