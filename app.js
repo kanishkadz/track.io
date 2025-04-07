d2a9c48180de59d3d4c5b70427a8a5ff742251d7
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-07 19:25:27 +0530

    front

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const path = require("path");
+
 const http = require("http");
 
 const socketio = require("socket.io");
@@ -8,6 +10,10 @@ const io  = socketio(server);
 
 app.set("view engine", "ejs");
 app.set(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function(socket){
+    console.log("connected");
+})
 
 app.get("/", function(req, res){
     res.send("hi");
