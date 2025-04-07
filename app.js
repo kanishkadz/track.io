1ca0c5c85fa1b0fb98ee8147dbfe95fd5b9ddc02
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-07 19:10:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express();
 const http = require("http");
 
 const socketio = require("socket.io");
-
 const server = http.createServer(app);
-
 const io  = socketio(server);
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
 
 app.get("/", function(req, res){
     res.send("hi");
