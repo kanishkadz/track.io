c3bc7c4dbdc4e4df7bfdb9d0c8b891846a9d927e
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-07 19:35:13 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,5 @@
 const socket = io();
 
-
 if(navigator.geolocation){
     navigator.geolocation.watchPosition((position) => {
         const { latitude, longitude } = position.coords;
@@ -15,3 +14,5 @@ if(navigator.geolocation){
 }
 
 L.map("map").setView([0, 0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
