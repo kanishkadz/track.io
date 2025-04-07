3bf878b28ddb353cb735acb36d61ae1bf128cb3e
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-07 19:53:28 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,51 @@
 const socket = io();
 
-if(navigator.geolocation){
-    navigator.geolocation.watchPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        socket.emit("send-location", {latitude, longitude});
-    }, (error) => {
-        console.error(error);
-    }, {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-    });
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit("send-location", { latitude, longitude });
+    },
+    (error) => {
+      console.error("Geolocation error:", error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    }
+  );
+} else {
+  alert("Geolocation is not supported by your browser.");
 }
 
-const map = L.map("map").setView([0, 0], 16);
+const map = L.map("map").setView([0, 0], 2); 
+
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "Kanishka Anand"
+  attribution: "&copy; OpenStreetMap contributors",
 }).addTo(map);
+
 
 const markers = {};
 
-socket.on("receive-location", (data) => {
-    const {id, latitude, longitude} = data;
+
+socket.on("receive-location", ({ id, latitude, longitude }) => {
+  if (!markers[id]) {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  } else {
+    markers[id].setLatLng([latitude, longitude]);
+  }
+
+  if (id === socket.id) {
     map.setView([latitude, longitude]);
-    if(markers[id]){
-        markers[id].setLatLng([latitude, longitude]);
-    } else {
-        markers[id] = L.marker([latitude, longitude]).addTo(map);
-    }
+  }
 });
 
+
 socket.on("user-disconnected", (id) => {
-    if(markers[id]){
-        map.removeLayer(markers[id]);
-        delete markers[id];
-    }
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
 });
