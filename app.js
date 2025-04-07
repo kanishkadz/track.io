3c38a186a7ddae4722425ae5b2cb3cafd219de27
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-07 19:40:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.set("view engine", "ejs");
 app.set(express.static(path.join(__dirname, "public")));
 
 io.on("connection", function(socket){
+    socket.on("send-location", function(data){
+        io.emit("receive-location", {id: socket.id, ...data});
+    });
     console.log("connected");
 })
 
