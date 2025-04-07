e5765e2b68fff2adecae80cb693aceec5f01f852
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-07 19:09:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require("http");
 
-const socketio = require("socketio");
+const socketio = require("socket.io");
 
 const server = http.createServer(app);
 
